6697b73528c02f2b0f6c9eef602433168b0ba3a0
stevengvan <vansteven17@gmail.com>
2023-02-01 16:10:48 -0800

    created ecommerce project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ sanity-ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gjwb6320',
+    dataset: 'production'
+  }
+})
